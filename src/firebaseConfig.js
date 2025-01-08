5e2a4e5778d2dbfd48991ba2ee05c192dafef15c
MOSTAFA <201129@ppu.edu.ps>
2025-01-08 16:14:45 +0200

    add .env file

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,23 +3,22 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// إعدادات Firebase
+// Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyAwkdHb7MJUBJWoPb2J6Z8v9pTFwU3S8sM",
-  authDomain: "senario-3184a.firebaseapp.com",
-  databaseURL: "https://senario-3184a-default-rtdb.firebaseio.com",
-  projectId: "senario-3184a",
-  storageBucket: "senario-3184a.appspot.com",
-  messagingSenderId: "14482505795",
-  appId: "1:14482505795:web:a4e2840cd7261f3c7394f6",
-  measurementId: "G-S63LJNEWDE"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
-// تهيئة التطبيق
+// Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
-// تصدير الكائنات التي سيتم استخدامها في أماكن أخرى
+// Export Firebase services
 export const auth = getAuth(firebaseApp);
 export const db = getFirestore(firebaseApp);
-export { firebaseApp }; // تصدير firebaseApp
-
+export { firebaseApp };
